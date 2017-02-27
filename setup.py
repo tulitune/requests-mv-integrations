5f609fc0b7e4a20723163b255617c5f19348e6f5
Jeff Tanner <jefft@tune.com>
2017-02-26 20:30:10 -0800

    [MVI-1098] (requests-mv-integrations) Python 3.5, v 0.04.2 (#75)

--- setup.py
+++ setup.py
@@ -59,7 +59,6 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: Implementation :: PyPy'
 ]
 
