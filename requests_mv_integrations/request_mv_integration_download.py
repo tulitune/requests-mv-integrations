aee571d8b19e6f11ad8df28c6d1073bc92e058f4
ohadtune <ohad@tune.com>
2017-03-02 09:43:55 +0200

    [MVI-1030] (request-mv-intergration) ValueError

--- requests_mv_integrations/request_mv_integration_download.py
+++ requests_mv_integrations/request_mv_integration_download.py
@@ -628,7 +628,7 @@ class RequestMvIntegrationDownload(object):
             json_file_content = json_file_r.read()
             try:
                 json_download = json.loads(json_file_content)
-            except json.decoder.JSONDecodeError as json_decode_ex:
+            except ValueError as json_decode_ex:
                 pprint(json_file_content)
 
                 response_extra.update({
